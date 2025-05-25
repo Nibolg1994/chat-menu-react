8be0b6745e9e9e86706982d4fe1a59cbe294f4d5
andrey <globinanvi@gmail.com>
2025-05-25 21:39:54 +0300

    added dish detail page

--- src/DishDetailPage.jsx
+++ src/DishDetailPage.jsx
@@ -6,7 +6,6 @@ import PrimaryButton from "./components/PrimaryButton.jsx";
 const DishDetailPage = () => {
     const location = useLocation();
     const navigate = useNavigate();
-
     const dish = location.state?.dish;
 
     if (!dish) return <div className="p-4">Блюдо не найдено</div>;
@@ -16,19 +15,19 @@ const DishDetailPage = () => {
     };
 
     return (
-        <div className="relative min-h-screen bg-white">
-            {/* Верхняя панель */}
+        <div className="relative min-h-screen bg-white text-gray-800">
+            {/* Назад */}
             <div className="absolute top-4 left-4 z-10">
                 <button
                     onClick={() => navigate(-1)}
-                    className="bg-white/80 backdrop-blur p-2 rounded-full shadow hover:scale-105 transition"
+                    className="bg-white/80 backdrop-blur p-2.5 rounded-lg shadow hover:scale-105 transition"
                 >
-                    <ArrowLeft size={20} />
+                    <ArrowLeft size={22} />
                 </button>
             </div>
 
             {/* Фото */}
-            <div className="h-64 w-full overflow-hidden rounded-b-3xl shadow-sm">
+            <div className="h-64 w-full overflow-hidden shadow-sm">
                 <img
                     src={dish.image}
                     alt={dish.title}
@@ -37,16 +36,23 @@ const DishDetailPage = () => {
             </div>
 
             {/* Контент */}
-            <div className="p-5">
-                <h1 className="text-2xl font-bold text-gray-800 mb-2">{dish.title}</h1>
+            <div className="p-6">
+                <h1 className="text-2xl font-semibold mb-4">{dish.title}</h1>
 
                 {dish.description && (
-                    <p className="text-gray-600 text-sm mb-4 leading-relaxed">
-                        {dish.description}
-                    </p>
+                    <div className="mb-6">
+
+                        <p className="text-gray-600 text-base leading-[1.8] whitespace-pre-line">
+                            {dish.description}  Описание
+                            Описание
+                            Описание Описание Описание Описание Описание Описание Описание
+                            Описание
+                        </p>
+                    </div>
                 )}
 
-                <div className="text-lg font-semibold text-brandBlue mb-6">
+                {/* Цена */}
+                <div className="text-xl font-semibold text-gray-800 mb-6">
                     {dish.price} ₽
                 </div>
 
