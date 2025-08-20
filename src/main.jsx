6cdd413e7dbba9922009a3c65cb8ff7082db70b1
Andrey Globin <globinav@rambler.ru>
2025-08-20 09:51:30 +0300

    integration react

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import './index.css'; // Tailwind
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
-        <BrowserRouter>
+        <BrowserRouter basename="/app">
             <App />
         </BrowserRouter>
     </React.StrictMode>
